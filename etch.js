a14df38fc9ff664e17f0faa47e442fdec99a0801
Khuhani <suleimanmigwi@gmail.com>
2025-04-15 16:51:55 +0300

    I realized that in the previous version, whenever I applied a new color to the squre divs, the entire container would adopt that color as well. In this version though, the container's background stays the same and only the square divs' color changes. Also, when entering the number of square divs in the input field, the divs populate without having to refresh the page. Only the rainbow feature is glitchy now, but that's pretty much a bonus feature. The main features work as intended, so I suppose I could pause here and perhaps review this project in the future, with more knowledge and experience.

--- etch.js
+++ etch.js
@@ -1,15 +1,54 @@
 const container = document.querySelector(".container");
 const numberOfSquares = document.querySelector(".square-num");
-//let numOfPixels = numberOfSquares.value;
+let size = numberOfSquares.value;
+const color = document.querySelector(".color");
+const clear = document.querySelector(".clear");
+const rainbow = document.querySelector(".rainbow");
 
 
+
+//logic for adding square divs to container//
 function populateContainer(size) {
     container.style.setProperty('--size',size);
+
     for(let i=0; i<size * size; i++) {
         const div = document.createElement("div");
         div.classList.add("pixels");
+        //logic for painting selected color//
+        div.addEventListener("mouseover", (e) => 
+            e.target.style.backgroundColor = color.value);
         container.appendChild(div);
     };
 };
 
-populateContainer(numberOfSquares.value);
+populateContainer(size);
+
+//logic for painting selected color//
+/*container.addEventListener("mouseover", (e) => e.target.style.backgroundColor = color.value);*/
+
+//logic for clearing color//
+function reset() {
+    container.innerHTML = "";
+    populateContainer(size);
+};
+
+clear.addEventListener("click", reset);
+
+//logic for changing number of square divs without page refresh//
+numberOfSquares.addEventListener("keyup", () => {size = numberOfSquares.value;
+    reset()});
+
+//logic for random color//
+function random(min, max) {
+    const num = Math.floor(Math.random() * (max - min)) + min;
+    return num;
+};
+
+function randomColor() {
+    return `rgb(${random(0, 255)} ${random(0, 255)} ${random(0, 255)})`;
+};
+
+rainbow.addEventListener("click",function(){
+    container.addEventListener("mouseover",(e) => 
+        e.target.style.backgroundColor = randomColor());
+});
